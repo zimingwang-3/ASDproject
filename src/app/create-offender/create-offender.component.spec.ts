6f8c39386283564404052d9c1574b185c44b9673
harrywhiteman <13564024@student.uts.edu.au>
2021-10-06 12:11:28 +1100

    testing imports

--- src/app/create-offender/create-offender.component.spec.ts
+++ src/app/create-offender/create-offender.component.spec.ts
@@ -1,6 +1,9 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { CreateOffenderComponent } from './create-offender.component';
+import { CreateOffenderService } from './create-offender.service';
 
 describe('CreateOffenderComponent', () => {
   let component: CreateOffenderComponent;
@@ -8,7 +11,9 @@ describe('CreateOffenderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateOffenderComponent ]
+      declarations: [ CreateOffenderComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
+      providers: [CreateOffenderService]
     })
     .compileComponents();
   });
